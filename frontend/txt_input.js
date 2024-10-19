1ff016246207f6c1692e825091f3ed69d71bcaac
Supreeth Sreekanth <supreethsreekanth@Supreeths-MacBook-Pro.local>
2024-10-18 22:47:57 -0700

    text input file

---
+++ frontend/txt_input.js
@@ -0,0 +1,28 @@
+// Description: input text field
+// Author: supreeth
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+export default function TextInput() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-helperText"
+          label="Helper text"
+          defaultValue="Default Value"
+          helperText="Some important text"
+        />
+    </Box>
+  );
+}
+
+TextInput();
