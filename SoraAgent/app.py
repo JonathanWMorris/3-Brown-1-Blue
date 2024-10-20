fb7c15d302dda64c7635eb94f9c9bb1a32152e28
Jonathan Morris <jonathan_wellington@icloud.com>
2024-10-20 02:10:52 +0000

    cooked

--- SoraAgent/app.py
+++ SoraAgent/app.py
@@ -4,6 +4,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Replace 'path/to/your/keyfile.json' with the actual path
 cred = credentials.Certificate('secret.json')
@@ -18,16 +20,15 @@ def get_sora():
     content = request.json
     prompt = content["prompt"]
     
-    code_to_execute = f'python scripts/inference.py configs/opensora-v1-2/inference/sample.py --num-frames 16s --resolution 144p --aspect-ratio 9:16 --llm-refine True --prompt "{prompt}"'
+    code_to_execute = f'python scripts/inference.py configs/opensora-v1-2/inference/sample.py --num-frames 4s --resolution 144p --aspect-ratio 9:16 --llm-refine True --prompt "{prompt}"'
     # exec(code_to_execute)
     
     i = os.system(f"cd ../../Open-Sora && {code_to_execute}")
     
     file_path = "../../Open-Sora/samples/samples/sample_0000.mp4"
     blob = bucket.blob(file_path)
-    file_name = "vid.mp4"
-    blob.upload_from_filename(filename = file_name)
-    return jsonify(path=file_name)
+    blob.upload_from_filename(filename = file_path)
+    return jsonify(path=file_path)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
